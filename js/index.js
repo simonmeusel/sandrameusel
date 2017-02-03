6a9ac64979b6a10112a2905e8efe3f3d131c49a6
Simon Meusel <simon.meusel.pc@t-online.de>
2017-02-03 20:19:01 +0100

    Add areas of interests

--- js/index.js
+++ js/index.js
@@ -4,24 +4,3 @@ function goto(target) {
         scrollTop: $(target).offset().top - 60
     }, 300);
 }
-
-$(window).on('resize', function() {
-    resize();
-});
-
-function resize() {
-    if ($(window).width() > 768) {
-        $('.sm').show();
-        $('.bottom').height($('.sm').height());
-    } else {
-        $('.bottom').height(50);
-        $('.sm').hide();
-    }
-}
-
-resize();
-setTimeout(resize, 200)
-
-$(window).load(function() {
-    resize();
-})
